cdfcff633ae424ae2f2c7365a625d50a0c56facc
yoshida-ryuho <srszu73t2mm@icloud.com>
2020-07-01 17:55:14 +0900

    chatspace-automatic-update-kansei

--- app/assets/javascripts/modules/user.js
+++ app/assets/javascripts/modules/user.js
@@ -1,7 +1,7 @@
 $(function() {
   function addUser(user) {
     let html = `
-                <div class="ChatMember clearfix">
+                <div class="ChatMember_clearfix">
                   <p class="ChatMember__name">${user.name}</p>
                   <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>
