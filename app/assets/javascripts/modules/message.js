58104ae0b5ae7072e0907017763551c08183212c
yoshida-ryuho <srszu73t2mm@icloud.com>
2020-07-07 18:49:50 +0900

    chatspace-s3-syusei

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -3,7 +3,6 @@ $(function(){
     if ( message.image ) {
       let html = 
         `<div class="MessageBox" data-message-id=${message.id}>
-          <div class="MessageBox">
             <div class="MessageInfo">
               <div class="MessageInfo__userName">
                 ${message.user_name}
@@ -24,7 +23,6 @@ $(function(){
     } else {
       let html =
       `<div class="MessageBox" data-message-id=${message.id}>
-        <div class="MessageBox">
           <div class="MessageInfo">
             <div class="MessageInfo__userName">
               ${message.user_name}
